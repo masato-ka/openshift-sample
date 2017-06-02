85d36c880373a03ff4bab4564a7fa8008338ff01
masato-ka <nsb21708@nifty.com>
2017-06-02 23:54:55 +0900

    initial commit

---
+++ src/main/java/ka/masato/openshift/OpenshiftSampleApplication.java
@@ -0,0 +1,12 @@
+package ka.masato.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/ka/masato/openshift/OpenshiftSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package ka.masato.openshift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenshiftSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
